497943d0a4a556d53864d0b181e1777ec9eddac0
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-02-21 18:56:20 +0530

    Update Language.h

--- Language.h
+++ Language.h
@@ -1,3 +1,3 @@
 #include "typewise-alert.h"
 
-const char TempAlertMessage[MaxNumOfSupportedLanguages][NumOfBreachTypes][100];
+extern const char TempAlertMessage[MaxNumOfSupportedLanguages][NumOfBreachTypes][100];
